cbf50391831c24e5ef18f650c4cc574077d1319b
Zachary Trouy <ztrouy@gmail.com>
2024-05-01 15:21:45 -0500

    Added GET endpoint that returns all Reserverations, embedding the User, Campsite, and Campsite Type

--- Program.cs
+++ Program.cs
@@ -108,4 +108,42 @@ app.MapPut("/api/campsites/{id}", (CreekRiverDbContext db, int id, Campsite camp
     return Results.NoContent();
 });
 
+app.MapGet("/api/reservations", (CreekRiverDbContext db) =>
+{
+    return db.Reservations
+        .Include(r => r.UserProfile)
+        .Include(r => r.Campsite)
+        .ThenInclude(c => c.CampsiteType)
+        .OrderBy(res => res.CheckinDate)
+        .Select(r => new ReservationDTO
+        {
+            Id = r.Id,
+            CampsiteId = r.CampsiteId,
+            UserProfileId = r.UserProfileId,
+            CheckinDate = r.CheckinDate,
+            CheckoutDate = r.CheckoutDate,
+            UserProfile = new UserProfileDTO
+            {
+                Id = r.UserProfile.Id,
+                FirstName = r.UserProfile.FirstName,
+                LastName = r.UserProfile.LastName,
+                Email = r.UserProfile.Email
+            },
+            Campsite = new CampsiteDTO
+            {
+                Id = r.Campsite.Id,
+                Nickname = r.Campsite.Nickname,
+                ImageUrl = r.Campsite.ImageUrl,
+                CampsiteTypeId = r.Campsite.CampsiteTypeId,
+                CampsiteType = new CampsiteTypeDTO
+                {
+                    Id = r.Campsite.CampsiteType.Id,
+                    CampsiteTypeName = r.Campsite.CampsiteType.CampsiteTypeName,
+                    MaxReservationDays = r.Campsite.CampsiteType.MaxReservationDays,
+                    FeePerNight = r.Campsite.CampsiteType.FeePerNight
+                }
+            }
+        }).ToList();
+});
+
 app.Run();
